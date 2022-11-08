1c443b7fd780601179018888407c017e728adb23
NaimaN00r <anaima407@gmail.com>
2022-11-09 00:23:14 +0600

    2nd commit

--- index.js
+++ index.js
@@ -10,16 +10,32 @@ const port = process.env.PORT || 4500;
 app.use(cors());
 app.use(express.json());
 
-console.log(process.env.DB_USER)
+
 
 const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.3b6qmgb.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+async function run(){
+    try{
+        const serviceCollection = client.db('photography').collection('services');
+        app.get('/services', async(req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query).limit(3);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+        app.get('/allservices', async(req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+    }
+    finally{
+
+    }
+
+}
+run().catch(err=>console.log(err));
 
 
 app.get('/', (req, res) => {
@@ -27,8 +43,7 @@ app.get('/', (req, res) => {
 });
 
 
-//GqGCjiTH0N4mQTwl--password
-//hlwmongo---usrName
+
 
 
 app.listen(port, () => {
